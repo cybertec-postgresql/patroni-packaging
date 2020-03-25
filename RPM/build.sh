b2339cf6c07a975aeeee92bda3e46ce773c122fa
Strahinja Kustudic <strahinjak@nordeus.com>
2020-03-25 12:55:09 +0100

    Fix rpms dir being created as root

--- RPM/build.sh
+++ RPM/build.sh
@@ -8,3 +8,6 @@ cp patches/*.patch /root/rpmbuild/SOURCES/
 rpmbuild --rpmfcdebug -bb patroni.spec
 mkdir -p rpms
 cp /root/rpmbuild/RPMS/x86_64/patroni-*.rpm rpms/
+if [ -n "$USER_ID" ]; then
+    chown -R $USER_ID:$USER_ID rpms
+fi
