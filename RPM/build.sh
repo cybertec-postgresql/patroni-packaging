dbbee758152385b9580f40f3a23d8372465fa031
Strahinja Kustudic <strahinjak@nordeus.com>
2020-03-25 10:36:25 +0100

    Improve changing the version for RPMs
    Now the version is only set in the spec file. Also when changing version
    we don't have to build the Docker image again, same image will work with
    all versions.

--- RPM/build.sh
+++ RPM/build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+
+set -e
+
 mkdir -p /root/rpmbuild/SOURCES
 tar -czf /root/rpmbuild/SOURCES/patroni-customizations.tar.gz patroni.2.service patroni-watchdog.service postgres-telia.yml 
 cp patches/*.patch /root/rpmbuild/SOURCES/
-curl -L https://github.com/zalando/patroni/archive/v1.6.4.tar.gz -o /root/rpmbuild/SOURCES/patroni-1.6.4.tar.gz
 rpmbuild --rpmfcdebug -bb patroni.spec
 mkdir -p rpms
 cp /root/rpmbuild/RPMS/x86_64/patroni-*.rpm rpms/
